3092712bbc430aa413ede1f8a921b76daa5b0237
Delrodie AMOIKON <delrodieamoikon@gmail.com>
2017-03-28 18:39:40 +0000

    Generation CRUD et mise en page de la classe Rechagent

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Entity\Recherche;
+use AppBundle\Entity\Rechagent;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -20,9 +21,15 @@ class DefaultController extends Controller
       $form = $this->createForm('AppBundle\Form\RechercheType', $recherche);
       $form->handleRequest($request);
 
+      $rechagent = new Rechagent();
+      $form_agent = $this->createForm('AppBundle\Form\RechagentType', $rechagent);
+      $form_agent->handleRequest($request);
+
         return $this->render('default/index.html.twig', array(
             'recherche' => $recherche,
             'form' => $form->createView(),
+            'rechagent' => $rechagent,
+            'form_agent' => $form_agent->createView(),
         ));
     }
 }
